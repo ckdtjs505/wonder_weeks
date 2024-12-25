d03bc8a07961cfd9f7a217739dd51604c9da3f27
ckdtjs505 <ckdtjs505@naver.com>
2024-12-25 17:39:55 +0900

    feat: 캘린더 한국어로 변경

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
 import { router } from "expo-router";
 import { Button, Text, View } from "react-native";
-import { Calendar } from "react-native-calendars";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+LocaleConfig.locales['ko'] = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+  dayNamesShort: ['월', '화', '수', '목', '금', '토', '일'],
+  today: "오늘"
+};
+
+LocaleConfig.defaultLocale = 'ko';
 
 export default function Home(){
   const handleModalButton = () => {
