40687cef16918189c0afadfd735923a73c5cc98a
ckdtjs505 <ckdtjs505@naver.com>
2024-12-25 17:37:53 +0900

    feat: 캘린더 추가하기

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { router } from "expo-router";
 import { Button, Text, View } from "react-native";
+import { Calendar } from "react-native-calendars";
 
 export default function Home(){
   const handleModalButton = () => {
@@ -8,6 +9,19 @@ export default function Home(){
 
   return <View>
     <Text>Home </Text>
+
+    <Calendar
+      markingType={'period'}
+      markedDates={{
+        '2024-12-20': {textColor: 'green'},
+        '2024-12-22': {startingDay: true, color: 'green'},
+        '2024-12-23': {selected: true, endingDay: true, color: 'green', textColor: 'gray'},
+        '2024-12-04': {disabled: true, startingDay: true, color: 'green', endingDay: true}
+      }}
+
+      enableSwipeMonths={true}
+      hideArrows={false}
+    />
     <Button title="모달" onPress={handleModalButton}></Button>
   </View>
 }
