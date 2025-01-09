d72a9bbb27fc338c502cb15a6bd423cabc05d90e
소니아_오창선 <ckdtjs505@sooplive.com>
2025-01-09 14:52:41 +0900

    💎 style(_layout): code formatting

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,17 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Redirect, router, Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Redirect, router, Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Alert, Button, Text, Touchable, TouchableOpacity, View } from 'react-native';
-import { useBabyInfo } from '@/store/store';
-import { Colors } from '@/constants/Colors';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  Alert,
+  Button,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useBabyInfo } from "@/store/store";
+import { Colors } from "@/constants/Colors";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 /**
  * app 레이아웃
@@ -20,85 +31,88 @@ import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
-    const baby = useBabyInfo()
-    const colorScheme = useColorScheme();
-    const [loaded] = useFonts({
-        SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
-    });
+  const baby = useBabyInfo();
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        if (loaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded]);
-
-    useEffect(() => {
-
-        // API 호출
-        const fetchData = async () => {
-            try {
-                await baby.initialize()
-            } catch (error) {
-                console.error('API 호출 중 오류 발생:', error);
-            } finally {
-                setLoading(false); // 로딩 상태 해제
-            }
-        };
-
-        fetchData();
-    }, [loading]);
-
-
-    if (!loaded && loading) {
-        return <Text> </Text>;
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
+  }, [loaded]);
 
-    if (!baby || baby.name === "") {
-        
-      return <Redirect href="/userInputModal" />;
-    }
+  useEffect(() => {
+    // API 호출
+    const fetchData = async () => {
+      try {
+        await baby.initialize();
+      } catch (error) {
+        console.error("API 호출 중 오류 발생:", error);
+      } finally {
+        setLoading(false); // 로딩 상태 해제
+      }
+    };
 
-    console.log(baby)
+    fetchData();
+  }, [loading]);
 
-    return (
-        <ThemeProvider value={colorScheme === 'dark' ? DefaultTheme : DefaultTheme}>
-            <Stack>
-                <Stack.Screen name="index" options={{
-                    headerShown: true,
-                    headerTitleAlign: 'center',
-                    title: `${baby.name}'s 원더윅스`,
-                    headerStyle: {
-                        backgroundColor: Colors.theme[1],
-                    },
-                    headerTitleStyle: {
-                        color: '#ffffff'
-                    },
-                    headerRight : (e) => 
-                        <TouchableOpacity
-                            onPressIn={ () =>router.push('/settings')}>
-                            <MaterialIcons   name='settings' color={"white"} size={20}></MaterialIcons> 
-                        </TouchableOpacity>
-                    ,
+  if (!loaded && loading) {
+    return <Text> </Text>;
+  }
 
-                }}>
-                </Stack.Screen>
-                <Stack.Screen name="settings" options={{
-                    headerShown: true,
-                    headerTitleAlign: 'center',
-                    title: '설정',
-                    headerStyle: {
-                        backgroundColor: Colors.theme[1],
-                    },
-                    headerTitleStyle: {
-                        color: '#ffffff'
-                    },
-                }}>
-                </Stack.Screen>
-                <Stack.Screen name="+not-found" />
-            </Stack>
-            <StatusBar style="auto" />
-        </ThemeProvider>
-    );
+  if (!baby || baby.name === "") {
+    return <Redirect href="/userInputModal" />;
+  }
+
+  console.log(baby);
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DefaultTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: true,
+            headerTitleAlign: "center",
+            title: `${baby.name}'s 원더윅스`,
+            headerStyle: {
+              backgroundColor: Colors.theme[1],
+            },
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+            headerRight: (e) => (
+              <TouchableOpacity onPressIn={() => router.push("/settings")}>
+                <MaterialIcons
+                  name="settings"
+                  color={"white"}
+                  size={20}
+                ></MaterialIcons>
+              </TouchableOpacity>
+            ),
+          }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="settings"
+          options={{
+            headerShown: true,
+            headerTitleAlign: "center",
+            title: "설정",
+            headerStyle: {
+              backgroundColor: Colors.theme[1],
+            },
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+          }}
+        ></Stack.Screen>
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
 }
